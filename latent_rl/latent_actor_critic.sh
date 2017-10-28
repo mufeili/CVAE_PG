de0088a9deca355f12b77f8dbbe21c7a60311c21
Mufei Li <mufeili1996@gmail.com>
2017-10-28 02:45:43 -0500

    Add files via upload

--- latent_rl/latent_actor_critic.sh
+++ latent_rl/latent_actor_critic.sh
@@ -1,3 +1,3 @@
 python3 actor_critic_acrobotv1.py --gamma 0.99 --seed 543 --num-episodes 120000\
-	--log-interval 10 --VAE-dir VAE_kl_dim\(z\)=4_discount=3e-5 --use-cuda 
+	--log-interval 10 --VAE-dir VAE_kl_dim\(z\)=4_discount=3e-5 --use-cuda\
 	--use-buffer --buffer-capacity 10000 --batch-size 128
