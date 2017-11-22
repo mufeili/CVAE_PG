2d2d36c2bf3a48a8b0593d27a1286750242fd7b4
Mufei Li <mufeili1996@gmail.com>
2017-11-22 16:22:47 +0800

    Delete vae_train.sh

--- latent_rl/vae_train.sh
+++
@@ -1,3 +0,0 @@
-python3 vae_example_s2s.py --batch-size 512 --iterations 10000 --epochs 20 \
-	--seed 543 --log-interval 10 --buffer-capacity 50000 --kl-divergence \
-	--policy-dir actor_critic_20171020-082409 --kl-weight 1e-5
