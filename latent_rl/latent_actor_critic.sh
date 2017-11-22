a7b01a16c8e72ce10f73fe9207849381c34a80f2
Mufei Li <mufeili1996@gmail.com>
2017-11-22 16:24:05 +0800

    Delete latent_actor_critic.sh

--- latent_rl/latent_actor_critic.sh
+++
@@ -1,3 +0,0 @@
-python3 actor_critic_acrobotv1.py --gamma 0.99 --seed 543 --num-episodes 120000\
-	--log-interval 10 --VAE-dir VAE_kl_dim\(z\)=4_discount=3e-3 --latent\
-	--use-cuda --log-dir discount=3e-3
