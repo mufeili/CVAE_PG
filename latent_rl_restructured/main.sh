f9ea9ca90fd8dff78b975115f56354127ea9c67f
Mufei Li <mufeili1996@gmail.com>
2017-12-11 15:30:04 +0800

    Add files via upload

--- latent_rl_restructured/main.sh
+++ latent_rl_restructured/main.sh
@@ -1,4 +1,5 @@
 python3 main.py --experiment 'a|s' --env Acrobot-v1 --gamma 0.99 --seed 543 \
-	--num-episodes 80000 --log-interval 10 --policy-lr 0.002 --z-dim 4 --vae-lr 5e-4 \
+	--num-episodes 20 --log-interval 10 --policy-lr 0.002 --z-dim 4 --vae-lr 5e-4 \
 	--buffer-capacity 5000 --batch-size 512 --vae-update-frequency 10 \
-	--vae-update-times 10 --kl-weight 3e-5 --bp2VAE
+	--vae-update-times 10 --vae-update-threshold 0.02 --vae-check-interval 500 \
+	--kl-weight 3e-5 --bp2VAE
