a3eb91147b69e7bb5299e7337d57617b78daa6ce
Mufei Li <mufeili1996@gmail.com>
2017-12-13 10:56:00 +0800

    Add files via upload

--- latent_rl_restructured/statistics.py
+++ latent_rl_restructured/statistics.py
@@ -35,14 +35,3 @@ for _ in range(len(record_dict['cum_reward'])):
     logger.scalar_summary('last_hundred_average', record_dict['last_hundred_average'][_], _ + 1)
     logger.scalar_summary('policy_loss', record_dict['policy_loss'][_], _ + 1)
     logger.scalar_summary('value_loss', record_dict['value_loss'][_], _ + 1)
-
-if 'mse_recon_loss' in item_list:
-    for _ in range(len(record_dict['mse_recon_loss'])):
-        logger.scalar_summary('mse_recon_loss', record_dict['mse_recon_loss'][_], _ + 1)
-        logger.scalar_summary('kl_loss', record_dict['kl_loss'][_], _ + 1)
-        logger.scalar_summary('vae_loss', record_dict['vae_loss'][_], _ + 1)
-elif 'mse_pred_loss' in item_list:
-    for _ in range(len(record_dict['mse_pred_loss'])):
-        logger.scalar_summary('mse_pred_loss', record_dict['mse_pred_loss'][_], _ + 1)
-        logger.scalar_summary('kl_loss', record_dict['kl_loss'][_], _ + 1)
-        logger.scalar_summary('vae_loss', record_dict['vae_loss'][_], _ + 1)
